b4bec3b8f405c3f08c735f05e0e3be0a39cc7b52
an956609 <an956609@dal.ca>
2018-02-05 18:09:42 -0400

    created Validator.java and associated unit test

---
+++ app/src/main/java/com/csci3130/assignment2/password_validator/Validator.java
@@ -0,0 +1,12 @@
+package com.csci3130.assignment2.password_validator;
+
+/**
+ * Created by agagne on 05/02/18.
+ */
+
+public class Validator {
+    public static int validate(String password) {
+
+        return 0;
+    }
+}
---
+++ app/src/test/java/com/csci3130/assignment2/password_validator/ValidatorTest.java
@@ -0,0 +1,10 @@
+package com.csci3130.assignment2.password_validator;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by agagne on 05/02/18.
+ */
+public class ValidatorTest {
+    
+}
