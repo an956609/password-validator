83a38ddab6466dd72cab5ce1874e710c19947c45
an956609 <an956609@dal.ca>
2018-02-06 16:14:10 -0400

    Created and modified layout for validation activity

---
+++ app/src/main/java/com/csci3130/assignment2/password_validator/MainActivity.java
@@ -0,0 +1,18 @@
+package com.csci3130.assignment2.password_validator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void checkPassword(View view) {
+
+    }
+}
